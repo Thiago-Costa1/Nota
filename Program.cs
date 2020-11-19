ceed6a6f517d42ed5ebf6616829a4ee46e9d9834
Thiago <thiagocostas2017@gmail.com>
2020-11-19 08:32:38 -0300

    Testado e funcionando corretamente

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Nota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+
+            do {
+                Console.Write("Digite uma nota entre 0 a 10: ");
+                nota = int.Parse(Console.ReadLine());         
+                              
+              
+
+              if(nota < 0 || nota > 10) {
+                  Console.WriteLine("Número Inválido");
+                  
+              } 
+
+              }while(!(nota >= 0 && nota <= 10));
+
+              Console.WriteLine("Número Aceito");
+        }
+    }
+}
